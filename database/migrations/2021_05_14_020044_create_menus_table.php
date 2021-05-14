028d522ef0c6ec5f1a352b3384e457a2d2dea017
wolf-disco <373738465@qq.com>
2021-05-15 01:06:29 +0800

    添加菜单表

---
+++ database/migrations/2021_05_14_020044_create_menus_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pid')->default(0)->comment('父级菜单id');
+            $table->string('name', 25)->default('')->comment('菜单名称');
+            $table->string('icon', 255)->default('')->comment('图标');
+            $table->timestamps();
+            $table->softDeletes()->comment('删除时间');
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_general_ci';
+        });
+
+        DB::statement("ALTER TABLE `menus` comment '菜单表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
